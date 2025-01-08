60c3e728200abe0dd1c900d80457b6b39e4ea908
aschnarre1 <aschnarre@gmail.com>
2025-01-07 16:51:01 -0800

    Add project files.

---
+++ BarryJBriggs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BarryJBriggs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

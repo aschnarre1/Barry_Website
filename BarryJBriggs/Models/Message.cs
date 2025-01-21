674d82c1243508a6a812ed6594be451712e5fed3
aschnarre1 <aschnarre@gmail.com>
2025-01-20 23:34:48 -0800

    Added Robot text file, minimum limits for data, css adjusting

--- BarryJBriggs/Models/Message.cs
+++ BarryJBriggs/Models/Message.cs
@@ -9,26 +9,28 @@ namespace BarryJBriggs.Models
         public int MessageId { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters long")]
         [StringLength(100, ErrorMessage = "Name must be less than 100 characters")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
 
 
         [Required(ErrorMessage = "Email is required")]
         [StringLength(100, ErrorMessage = "Email must be less than 100 characters")]
         [EmailAddress(ErrorMessage = "Invalid email address format")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [StringLength(200, ErrorMessage = "Website must be less than 200 characters")]
         [Url(ErrorMessage = "Invalid website URL format")]
-        [RegularExpression(@"^(https?://.*)?$", ErrorMessage = "Invalid website URL format")]
+        [RegularExpression(@"^(https?:\/\/.*)?$", ErrorMessage = "Invalid website URL format")]
 
         public string? Website { get; set; }
 
 
         [Required(ErrorMessage = "Message is required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 10 characters long")]
         [StringLength(500, ErrorMessage = "Message must be less than 500 characters")]
-        public string MessageText { get; set; }
+        public required string MessageText { get; set; }
 
 
     }

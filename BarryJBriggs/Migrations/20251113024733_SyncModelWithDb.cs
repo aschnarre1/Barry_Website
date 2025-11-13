5d03d5707d853a3a8a4af982a8416f2aff5657c4
aschnarre <aschn@AUSTINS_PC>
2025-11-12 18:48:07 -0800

    Add migrations to sync model and database

---
+++ BarryJBriggs/Migrations/20251113024733_SyncModelWithDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarryJBriggs.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelWithDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "AboutPages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "AboutPages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
